3de90c6f0226422af2bfa2f24a65178a29dc33bb
zhonganping <mysticzap@qq.com>
2022-10-19 14:52:59 +0800

    '优化'

--- src/tnet/v2/callrecording/DescribeRecordFileUrl.php
+++ src/tnet/v2/callrecording/DescribeRecordFileUrl.php
@@ -7,11 +7,12 @@ namespace mysticzap\tinetclink\tnet\v2\callrecording;
 use mysticzap\tinetclink\tnet\v2\Api;
 
 /**
- * 6.3.1. 下载通话录音文件
- * @link [third api doc]https://develop.clink.cn/develop/api/cc.html#_下载通话录音文件
+ * 6.3.2. 查看通话录音地址
+ * @link [third api doc]https://develop.clink.cn/develop/api/cc.html#_查看通话录音地址
+ *
  * @package mysticzap\tinetclink\tnet\v2\callrecording
  */
-class DownloadRecordFile extends Api
+class DescribeRecordFileUrl extends Api
 {
     /**
      * @var array 请求包含的参数，以及是否必填
@@ -21,10 +22,12 @@ class DownloadRecordFile extends Api
         "mainUniqueId" => true,
         // 不传递获取mp3格式录音，传递时获取wav格式录音。1：双轨录音客户侧，2：双轨录音座席侧，3：两侧合成录音
         "recordSide" => false,
+        // 获取录音地址超时时长，单位为秒，默认为一小时，范围在一到二十四小时。
+        "timeout" => false,
     ];
 
-    public $apiName = "下载通话录音文件";
-    public $api = '/cc/download_record_file';
+    public $apiName = "查看通话录音地址";
+    public $api = '/cc/describe_record_file_url';
     public $method = self::METHOD_GET;
 
 }
